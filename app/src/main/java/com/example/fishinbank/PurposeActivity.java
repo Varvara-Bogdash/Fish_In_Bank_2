c997f6907d3cdbae6d6ce870ca0e117bef0aae5d
Test <varvara.bogdan10@gmail.com>
2025-12-14 17:40:21 +0300

    добавлено изменение картинки, вылетает при создании суммы

--- app/src/main/java/com/example/fishinbank/PurposeActivity.java
+++ app/src/main/java/com/example/fishinbank/PurposeActivity.java
@@ -1,15 +1,17 @@
 package com.example.fishinbank;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PurposeActivity extends AppCompatActivity {
@@ -24,6 +26,8 @@ public class PurposeActivity extends AppCompatActivity {
     int minusAllCount;
     int count;
     int allCount;
+    ImageView cupImage;
+    int countOfClick = 0;
 
 
     ActivityResultLauncher<Intent> addPurposeLauncher = registerForActivityResult(
@@ -35,14 +39,18 @@ public class PurposeActivity extends AppCompatActivity {
                         Intent data = result.getData();
                         allMoney = data.getStringExtra("allMoney");
                         money = data.getStringExtra("money");
-                        allCount+=Integer.parseInt(allMoney);
-                        count+=Integer.parseInt(money);
+                        allCount = allCount + Integer.parseInt(allMoney);
+                        count = count + Integer.parseInt(money);
                         // Обновляем TextView с нужными надписями и значениями
                         if (allMoney != null && !allMoney.isEmpty()) {
                             allCountText.setText("Желаемая сумма: " + String.valueOf(allCount));
                         }
                         if (money != null && !money.isEmpty()) {
                             countText.setText("Накоплено: " + String.valueOf(count));
+                        }
+                        if(count>=allCount){
+                            cupImage.setImageResource(R.drawable.cup_all);
+                            countOfClick=0;
                         }
                     }
 
@@ -56,15 +64,20 @@ public class PurposeActivity extends AppCompatActivity {
 
         allCountText = findViewById(R.id.allCountText);
         countText = findViewById(R.id.cointText);
+        cupImage = findViewById(R.id.cupImage); // Инициализируем ImageView
         allCountText.setFreezesText(true);
         countText.setFreezesText(true);
         saveClass = new SaveClass();
-        saveClass.saveText(countText, allCountText, allMoney, money);
+        cupImage.setImageResource(R.drawable.cup);
     }
 
     public void addCount(View view) {
         Intent intent = new Intent(this, AddPurposeActivity.class);
         addPurposeLauncher.launch(intent);
+        if(count>=allCount){
+            cupImage.setImageResource(R.drawable.cup_all);
+            countOfClick=0;
+        }
     }
 
     public void home(View view) {
@@ -84,25 +97,88 @@ public class PurposeActivity extends AppCompatActivity {
 
     public void minusCount(View view) {
         minusCount -= 500;
-        count = plusCount + minusCount;
-        countText.setText(String.valueOf(count));
+        count = Integer.parseInt(plusCount + minusCount + money);
+        countText.setText("Накоплено: " + String.valueOf(count));
+        countOfClick++;
+        if(countOfClick <= 5) {
+            cupImage.setImageResource(R.drawable.cup_add);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        } else if (countOfClick <= 10) {
+            cupImage.setImageResource(R.drawable.cup_one_quarter);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        } else if (countOfClick<=15) {
+            cupImage.setImageResource(R.drawable.cup_half);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        } else {
+            cupImage.setImageResource(R.drawable.cup_three_quarter);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        }
+
+
     }
 
     public void plusCount(View view) {
         plusCount += 1000;
-        count = plusCount + minusCount;
-        countText.setText(String.valueOf(count));
+        count = Integer.parseInt(plusCount + minusCount + money);
+        countText.setText("Накоплено: " + String.valueOf(count));
+        countOfClick++;
+        if(countOfClick <= 5) {
+            cupImage.setImageResource(R.drawable.cup_add);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        } else if (countOfClick <= 10) {
+            cupImage.setImageResource(R.drawable.cup_one_quarter);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        } else if (countOfClick<=15) {
+            cupImage.setImageResource(R.drawable.cup_half);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        } else {
+            cupImage.setImageResource(R.drawable.cup_three_quarter);
+            if(count>=allCount){
+                cupImage.setImageResource(R.drawable.cup_all);
+                countOfClick=0;
+            }
+        }
+
     }
 
     public void plusAllCount(View view) {
         plusAllCount += 1000;
-        allCount = plusAllCount + minusAllCount;
-        allCountText.setText(String.valueOf(allCount));
+        allCount = Integer.parseInt(plusAllCount + minusAllCount + allMoney);
+        allCountText.setText("Желаемая сумма: " + String.valueOf(allCount));
+        if(count>=allCount){
+            cupImage.setImageResource(R.drawable.cup_all);
+            countOfClick=0;
+        }
     }
 
     public void minusAllCount(View view) {
         minusAllCount -= 500;
-        allCount = plusAllCount + minusAllCount;
-        allCountText.setText(String.valueOf(allCount));
+        allCount = Integer.parseInt(plusAllCount + minusAllCount + allMoney);
+        allCountText.setText("Желаемая сумма: " + String.valueOf(allCount));
+        if(count>=allCount){
+            cupImage.setImageResource(R.drawable.cup_all);
+            countOfClick=0;
+        }
     }
 }

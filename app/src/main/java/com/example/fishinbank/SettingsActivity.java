2d20e4833dda0ed2425e7236eb9f82668c2fe59a
Test <varvara.bogdan10@gmail.com>
2026-03-11 19:11:37 +0300

    смена фона

--- app/src/main/java/com/example/fishinbank/SettingsActivity.java
+++ app/src/main/java/com/example/fishinbank/SettingsActivity.java
@@ -1,23 +1,36 @@
-// SettingsActivity.java
 package com.example.fishinbank;
 
 import android.os.Bundle;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
+
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class SettingsActivity extends AppCompatActivity {
+    ConstraintLayout layout;
 
-
+    private boolean isColorMode = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        layout = findViewById(R.id.root_layout);
+    }
+    public void fonChange(View view) {
+        if (!isColorMode) {
+            layout.setBackgroundResource(R.drawable.new_fon);
+        } else {
+            layout.setBackgroundResource(R.drawable.fon);
+        }
+        isColorMode = !isColorMode; // переключение флага
     }
 
-    public void fonChange(View view) {
+    public void home(View view) {
 
     }
 }
